d29cf078b72967cb1f8049ef578ea53d4838d325
ahmedmedhat163 <128075380+ahmedmedhat163@users.noreply.github.com>
2023-04-04 00:01:44 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let Menu = document.querySelector("#MenuBtn");
+let Navbar = document.querySelector(".navbar");
+
+Menu.onclick = () => {
+  Menu.classList.toggle("fa-times");
+  Navbar.classList.toggle("active");
+  ThemeToggle.classList.remove("active");
+};
+
+window.onscroll = () => {
+  Menu.classList.remove("fa-times");
+  Navbar.classList.remove("active");
+  ThemeToggle.classList.remove("active");
+};
+
+// Theme Toggle
+let ThemeToggle = document.querySelector(".themeToggle");
+let ToggleBtn = document.querySelector(".ToggleBtn");
+
+ToggleBtn.onclick = () => {
+  ThemeToggle.classList.toggle("active");
+  Menu.classList.remove("fa-times");
+  Navbar.classList.remove("active");
+};
+document.querySelectorAll(".themeToggle .themeBtn").forEach((btn) => {
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector(":root").style.setProperty("--main", color);
+  };
+});
+
+
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
